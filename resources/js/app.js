1bf978389a291db3cdc6b308234b6a3cf64e680e
Samanta Inzerilli <samantainzerilli@gmail.com>
2023-07-14 15:18:10 +0200

    1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,40 @@
 import './bootstrap';
+import 'fullcalendar/dist/fullcalendar.css';
+import 'fullcalendar';
 
 $(document).ready(function() {
-    // Evento para abrir el modal y agregar un nuevo evento
-    $('#addEventButton').click(function() {
-      $('#eventModalLabel').text('Agregar Evento');
-      $('#eventForm')[0].reset(); // Limpiar el formulario
-      $('#eventModal').modal('show');
-    });
-  
-    // Evento para enviar el formulario y guardar un nuevo evento
-    $('#eventForm').submit(function(e) {
-      e.preventDefault();
-      var formData = $(this).serialize();
-      $.ajax({
-        url: '/events',
-        type: 'POST',
-        data: formData,
-        success: function(response) {
-          $('#eventModal').modal('hide');
-          // Aquí puedes agregar la lógica para actualizar el calendario con el nuevo evento
-        },
-        error: function(xhr) {
-          // Manejar errores de validación o cualquier otro error
-        }
-      });
+  // Evento para abrir el modal y agregar un nuevo evento
+  $('#addEventButton').click(function() {
+    $('#eventModalLabel').text('Agregar Evento');
+    $('#eventForm')[0].reset(); // Limpiar el formulario
+    $('#eventModal').modal('show');
+  });
+
+  // Evento para enviar el formulario y guardar un nuevo evento
+  $('#eventForm').submit(function(e) {
+    e.preventDefault();
+    var formData = $(this).serialize();
+    $.ajax({
+      url: '/events',
+      type: 'POST',
+      data: formData,
+      success: function(response) {
+        $('#eventModal').modal('hide');
+        // Aquí puedes agregar la lógica para actualizar el calendario con el nuevo evento
+      },
+      error: function(xhr) {
+        // Manejar errores de validación o cualquier otro error
+      }
     });
   });
-  
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      events: '/calendario'
+    });
+    calendar.render();
+  });
+});
 
