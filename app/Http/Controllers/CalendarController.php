2496cacdfb8ad908c7ef3fcfe5aa0c08ef33667c
Samanta Inzerilli <samantainzerilli@gmail.com>
2023-07-14 19:22:39 +0200

    modific

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -1,14 +1,25 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Evento;
 
 class CalendarController extends Controller
 {
     public function index()
     {
-        return view('calendar');
+        $events = [];
+        $data = Evento::all();
+        if($data->count()){
+            foreach ($data as $key => $value) {
+                $events[] = [
+                    'title' => $value->titulo,
+                    'start' => $value->fecha_inicio,
+                    'end' => $value->fecha_fin
+                ];
+            }
+        }
+        return view('calendar', compact('events'));
     }
 }
 
